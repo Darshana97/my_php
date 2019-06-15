19360895203fbc04ab2acbf2bdd653025b072d1d
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-06-16 00:25:19 +0530

    arithmatic operators
    arithmatic operators in php

---
+++ arithmaticoperator.php
@@ -0,0 +1,20 @@
+<?php
+
+$num1 = 10;
+$num2 = 3;
+
+$result1 = $num1 + $num2;
+$result2 = $num1 - $num2;
+$result3 = $num1 * $num2;
+$result4 = $num1 / $num2;
+
+echo "sum is '.$result1.' <br>";
+echo "sub is '.$result2.' <br>";
+echo "mul is $result3 <br>";
+echo "div is $result4 <br>";
+
+
+
+
+
+?>
