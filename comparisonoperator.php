070e1c2d88329de3cfb1e530cdc70f6abb358981
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-06-18 20:30:49 +0530

    comparisonoperator
    comparisonoperator in php

---
+++ comparisonoperator.php
@@ -0,0 +1,24 @@
+<?php
+
+$age = 20;
+$status = false;
+
+if($age>=22)
+{
+    $status = true;
+}
+
+if($status==true)
+{
+    echo 'you can drink arrack';
+}
+else
+{
+    echo "you can't drink arrack";
+}
+
+
+
+
+
+?>
