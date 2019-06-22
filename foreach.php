528c925c63a6d5a4185f29877cff3fb620244406
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-06-22 19:56:12 +0530

    foreach
    foreach in php

---
+++ foreach.php
@@ -0,0 +1,38 @@
+<?php
+
+	/*$names = array('Alex','Billy','Dale');
+
+	foreach ($names as $value) {
+
+		echo $value;
+		echo "<br>";
+	}*/
+
+
+
+
+	/*$num = 1;
+	$names = array('Alex','Billy','Dale');
+
+	foreach ($names as  $value) {
+
+
+		echo "name '.$num.' is '.$value.'";
+		echo "<br>";
+	}*/
+
+
+
+	$names = array('Alex'=>21,'Billy'=>16,'Dale'=>49);
+
+	foreach ($names as $key => $value) {
+
+		echo "'.$key.' is '.$value.'";
+		echo "<br>";
+	}
+
+
+
+
+
+?>
