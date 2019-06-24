6d6190f00e84a34efe2dfc2d75b864da5e691622
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-06-24 20:57:05 +0530

    function
    function in php

---
+++ function.php
@@ -0,0 +1,42 @@
+<?php
+
+	/*function name($names)
+	{
+		echo " my name is: '.$names.'";
+	}
+
+	 name("darshana");
+*/
+
+
+	/* function age($age)
+	 {
+	 	return "my age is: $age";
+
+	 }
+
+	 $returnvalue = age(22);
+	 echo($returnvalue);*/
+
+
+
+	/* function details($name,$age)
+	 {
+	 	return "my name is $name and my age is $age";
+	 }
+
+	 $returndetails = details("brayan",22);
+	 echo($returndetails);*/
+
+
+
+	 function add($num1,$num2)
+	 {
+	 	$result = $num1 + $num2;
+	 	return $result;
+	 }
+
+	 echo add(10,3);
+
+
+?>
